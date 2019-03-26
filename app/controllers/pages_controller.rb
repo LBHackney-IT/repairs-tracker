df18a40563b9d688683dad9a322b1e8e58a47a49
Celia Collins <celiadcollins@gmail.com>
2019-03-26 17:34:28 +0000

    Setup simple authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
+  before_action :authenticate
+
   def home
   end
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "admin" && password == "celiathebest"
+    end
+  end
+
 end
