207522b7b341bbf7522e81c91279e64afaf36f09
Celia Collins <celiadcollins@gmail.com>
2019-04-01 14:23:24 +0100

    Add property controller and model

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,26 @@
+class PropertiesController < ApplicationController
+  include PropertyHelper
+  rescue_from HackneyAPI::RepairsClient::RecordNotFoundError, with: :routing_error
+
+  def show
+    @property_details = Hackney::Property.find(params[:ref])
+  end
+
+  def search
+    @limit = 201
+    @address_list =
+      if params[:ref].present?
+        if is_postcode?(params[:ref])
+          Hackney::Property.for_postcode(params[:ref])
+        else
+          Hackney::Property.for_address(params[:ref], limit: @limit)
+        end
+      end
+  end
+
+  private
+
+  def routing_error
+    raise ActionController::RoutingError.new('Not Found')
+  end
+end
