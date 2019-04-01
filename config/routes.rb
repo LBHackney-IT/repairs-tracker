0e5d8daed453f5d0f27b8c756365aee75872a8ee
Celia Collins <celiadcollins@gmail.com>
2019-04-01 15:49:41 +0100

    Whole work order page without css

--- config/routes.rb
+++ config/routes.rb
@@ -7,23 +7,4 @@ Rails.application.routes.draw do
   resources :work_orders, only: [:show], param: :ref do
     post :search, on: :collection
   end
-
-  namespace :api do
-    resources :properties, only: [], param: :ref do
-      member do
-        get :repairs_history
-      end
-    end
-
-    resources :work_orders, only: [], param: :ref do
-      member do
-        get :description
-        get :documents
-        get :notes_and_appointments
-        get :possibly_related_work_orders
-        get :related_work_orders
-        post :notes
-      end
-    end
-  end
 end
